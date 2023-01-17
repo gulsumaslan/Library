ee556bac4e423d8a0c52ae4dc1e73b95a32ff45f
Gülsüm KAYACAN <54817521+gulsumaslan@users.noreply.github.com>
2023-01-17 21:51:10 +0300

    Update Program.cs

--- Library/Program.cs
+++ Library/Program.cs
@@ -10,18 +10,18 @@ namespace Library
         {
             List<Books> book = new List<Books>
             {
-                new Books{ID=1,Ad=Messages.Adi1,KitapYazar=Messages.Yazar1,KitapYayinevi=Messages.Yayinevi1,KitapStok=100},
-                new Books{ID=1,Ad=Messages.Adi2,KitapYazar=Messages.Yazar2,KitapYayinevi=Messages.Yayinevi2,KitapStok=1000},
-                new Books{ID=1,Ad=Messages.Adi3,KitapYazar=Messages.Yazar2,KitapYayinevi=Messages.Yayinevi2,KitapStok=500},
-                new Books{ID=1,Ad=Messages.Adi4,KitapYazar=Messages.Yazar2,KitapYayinevi=Messages.Yayinevi2,KitapStok=100},
-                new Books{ID=1,Ad=Messages.Adi5,KitapYazar=Messages.Yazar3,KitapYayinevi=Messages.Yayinevi3,KitapStok=100},
+                new Books{ID=1,Name=Messages.Name1,BookAuthor=Messages.Author1,BookPublisher=Messages.Publisher1,BookStock=100},
+                new Books{ID=1,Name=Messages.Name2,BookAuthor=Messages.Author2,BookPublisher=Messages.Publisher2,BookStock=1000},
+                new Books{ID=1,Name=Messages.Name3,BookAuthor=Messages.Author2,BookPublisher=Messages.Publisher2,BookStock=500},
+                new Books{ID=1,Name=Messages.Name4,BookAuthor=Messages.Author2,BookPublisher=Messages.Publisher2,BookStock=100},
+                new Books{ID=1,Name=Messages.Name5,BookAuthor=Messages.Author3,BookPublisher=Messages.Publisher3,BookStock=100},
 
             };
 
-            var results = book.Where(b => b.ID == 1 && b.KitapYazar ==Messages.Yazar2);
+            var results = book.Where(b => b.ID == 1 && b.KitapYazar ==Messages.Author2);
             foreach (var result in results)
             {
-                Console.WriteLine(result.Ad);
+                Console.WriteLine(result.Name);
 
             }
 
@@ -29,33 +29,33 @@ namespace Library
 
             List<Publisher> publishers = new List<Publisher>
             {
-                new Publisher{YazarID=1,YayineviID=2,YayineviAd=Messages.Yayinevi4, YazarAD=Messages.Yazar3},
-                new Publisher{YazarID=1,YayineviID=2,YayineviAd=Messages.Yayinevi4, YazarAD=Messages.Yazar4},
-                new Publisher{YazarID=1,YayineviID=2,YayineviAd=Messages.Yayinevi3, YazarAD=Messages.Yazar4},
-                new Publisher{YazarID=1,YayineviID=2,YayineviAd=Messages.Yayinevi1, YazarAD=Messages.Yazar5},
-                new Publisher{YazarID=1,YayineviID=2,YayineviAd=Messages.Yayinevi1, YazarAD=Messages.Yazar5},
+                new Publisher{AuthorID=1,PublisherID=2,PublisherName=Messages.Publisher4, AuthorName=Messages.Author3},
+                new Publisher{AuthorID=1,PublisherID=2,PublisherName=Messages.Publisher4, AuthorName=Messages.Author4},
+                new Publisher{AuthorID=1,PublisherID=2,PublisherName=Messages.Publisher3, AuthorName=Messages.Author4},
+                new Publisher{AuthorID=1,YPublisherID=2,PublisherName=Messages.Publisher1, AuthorName=Messages.Author5},
+                new Publisher{AuthorID=1,PublisherID=2,PublisherName=Messages.Publisher1, AuthorName=Messages.Author5},
 
             };
-            var results2 = publishers.Where(p => p.YayineviAd == Messages.Yayinevi1);
+            var results2 = publishers.Where(p => p.PublisherName == Messages.Publisher1);
             foreach (var result2 in results2)
             {
-                Console.WriteLine(result2.YayineviID);
+                Console.WriteLine(result2.PublisherID);
             }
 
 
             List<BookType> bookTypes = new List<BookType>
             {
-                new BookType{TurID=3,TurAD=Messages.tur1},
-                new BookType{TurID=3,TurAD=Messages.tur2},
-                new BookType{TurID=2,TurAD=Messages.tur3},
-                new BookType{TurID=1,TurAD=Messages.tur4},
+                new BookType{GenusID=3,GenusName=Messages.genus1},
+                new BookType{GenusID=3,GenusName=Messages.genus2},
+                new BookType{GenusID=2,GenusName=Messages.genus3},
+                new BookType{GenusID=1,GenusName=Messages.genus4},
 
             };
 
-            var results4 = bookTypes.Where(b => b.TurID > 2);
+            var results4 = bookTypes.Where(b => b.GenusID > 2);
             foreach (var result4 in results4)
             {
-                Console.WriteLine(result4.TurAD);
+                Console.WriteLine(result4.GenusName);
 
             }
 
@@ -67,27 +67,27 @@ namespace Library
 
     public static class Messages
     {
-        public static string Adi1 = "Sırlar kitabi";
-        public static string Adi2 = "Bin Muhteşem güneş";
-        public static string Adi3 = "Uçurtma Avcısı";
-        public static string Adi4 = "Deniz Duası";
-        public static string Adi5 = "Kader Kuyusu";
+        public static string Name1 = "Sırlar kitabi";
+        public static string Name2 = "Bin Muhteşem güneş";
+        public static string Name3 = "Uçurtma Avcısı";
+        public static string Name4 = "Deniz Duası";
+        public static string Name5 = "Kader Kuyusu";
 
-        public static string Yayinevi1= "Omega Yayınevi";
-        public static string Yayinevi2 = "Everest Yayınevi";
-        public static string Yayinevi3 = "İthaki Yayınevi";
-        public static string Yayinevi4 = "Yeni nesil Yayınevi";
+        public static string Publisher1= "Omega Yayınevi";
+        public static string Publisher2 = "Everest Yayınevi";
+        public static string Publisher3 = "İthaki Yayınevi";
+        public static string Publisher4 = "Yeni nesil Yayınevi";
 
-        public static string Yazar1 = "Osho";
-        public static string Yazar2 = "Khaled Hosseini";
-        public static string Yazar3= "Mehmet Uzun";
-        public static string Yazar4 = "Ahmet Arif";
-        public static string Yazar5 = "Cemal Süreyya";
+        public static string Author1 = "Osho";
+        public static string Author2 = "Khaled Hosseini";
+        public static string Author3= "Mehmet Uzun";
+        public static string Author4 = "Ahmet Arif";
+        public static string Author5 = "Cemal Süreyya";
 
-        public static string tur1 = "Roman";
-        public static string tur2 = "Hikaye";
-        public static string tur3 = "Makale";
-        public static string tur4 = "Şiir";
+        public static string genus1 = "Roman";
+        public static string genus2 = "Hikaye";
+        public static string genus3 = "Makale";
+        public static string genus4 = "Şiir";
 
     }
 }
